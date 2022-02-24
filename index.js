e62d08d9dc782f9a4e61bd8545a5b1ff7c648ae4
tebebeyesus garedew <tebebe@eboltsolutions.com>
2022-02-23 21:40:26 -0500

    added the necessary changes so that page displays right values

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+console.log("hello")
+for(let i =0; i < weeklyExpenseQuestions.length; i++)
+{
+    console.log("hello")
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer;
+}
+
+
+for(let i =0; i < monthlyExpenseQuestions.length; i++)
+{
+    console.log("hello")
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer;
+}
+
+
+for(let i =0; i < annualExpenseQuestions.length; i++)
+{
+    console.log("hello")
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer;
+}
+
+
